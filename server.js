f27f0bda06c29022d6109d3fbee2956a7acc5bf2
Hansel <hanselcolmenarez@hotmail.com>
2023-05-19 17:46:45 -0400

    Se crea linea para correr en modo seguro la app

---
+++ server.js
@@ -0,0 +1,20 @@
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const options = {
+  key: fs.readFileSync('/opt/ssl-certs/key.pem'),
+  cert: fs.readFileSync('/opt/ssl-certs/cert.pem'),
+};
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+https.createServer(options, app).listen(443, () => {
+  console.log('Servidor HTTPS iniciado en el puerto 443');
+});
--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
 		let options = {
 			model: 'text-davinci-003',
 			temperature: 0,
-			max_tokens: 300,
+			max_tokens: 1024,
 			top_p: 1,
 			frequency_penalty: 0.0,
 			presence_penalty: 0.0,
