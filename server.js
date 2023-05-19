ea75be16f7467afac9b104ae3c0d1c5dd68f9538
Hansel <hanselcolmenarez@hotmail.com>
2023-05-19 18:17:56 -0400

    se actualiza index.html a js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const options = {
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.js'));
 });
 
 https.createServer(options, app).listen(443, () => {
