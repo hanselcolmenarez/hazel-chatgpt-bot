06ce7a29e898a47138c5aeeedaef98bfaeb36b0e
Hansel <hanselcolmenarez@hotmail.com>
2023-05-19 23:59:58 -0400

    intento de iniciar en modo https

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+// Configura los middlewares y rutas de tu aplicación
+
+// Obtén la ruta del certificado y la clave privada
+const certPath = path.resolve(__dirname, 'cert.pem');
+const keyPath = path.resolve(__dirname, 'key.pem');
+
+// Crea el servidor HTTPS
+const server = https.createServer(
+  {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath),
+  },
+  app
+);
+
+// Inicia el servidor
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Servidor HTTPS iniciado en el puerto ${port}`);
+});
